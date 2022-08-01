cb19361cc3e9a940fe8b66aa684ca666f080bcc1
Oscar Gonzalez simon <ogonzale@cbr4s5.42barcelona.com>
2022-08-01 18:59:39 +0200

    Add tutorial

--- src/mand/pipex.c
+++ src/mand/pipex.c
@@ -6,7 +6,7 @@
 /*   By: ogonzale <ogonzale@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 11:23:54 by ogonzale          #+#    #+#             */
-/*   Updated: 2022/08/01 13:28:56 by ogonzale         ###   ########.fr       */
+/*   Updated: 2022/08/01 18:59:05 by ogonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,48 +15,43 @@
 #include "utils.h"
 #include "libft.h"
 #include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
 
-const char *process = NULL;
-
-void	read_from_fd(int fd)
-{
-	char	c;
-	ft_printf("Process [%s] Reading from descriptor %d \n", process, fd);
-	while (read(fd, &c, 1) != 0)
-		ft_printf("%c", c);
-}
+// Remember to treat errors for fork, read, write, etc.
+// Add wait to end of parent process (use while loop if there are multiple
+// children processes.
+// Use O_WRONLY | O_CREATE flags to redirect standard output to outfile.
 
 int	main(int argc, char *argv[])
 {
+	int	id;
 	int	fd[2];
-	int	pid;
-	const char *data;
 
-	process = "PARENT";
+	// fd[0] - read
+	// fd[1] - write
 	if (argc < 5)
 		terminate(ERR_ARGS);
-	if (pipe(fd) != 0)
+	if (pipe(fd) == -1)
 		terminate(ERR_PIPE);
-	pid = fork();
-	if (pid == -1)
-		terminate(ERR_PIPE);
-	else if (pid == 0)
+	id = fork();
+	if (id == 0)
 	{
-		process = "CHILD";
+		close(fd[0]);
+		int	x;
+		printf("Input a number: ");
+		scanf("%d", &x);
+		write(fd[1], &x, sizeof(int));
 		close(fd[1]);
-		read_from_fd(fd[0]);
-		exit(0);
 	}
 	else
 	{
-		close(fd[0]);
-		data = "Data";
-		ft_printf("Process [%s] Writing data to pipe \n", process);
-		ft_printf("pid = %d\n", pid);
-		write(fd[1], data, ft_strlen(data));
 		close(fd[1]);
-		waitpid(pid, 0, 0);
-		exit(0);
+		int	y;
+		read(fd[0], &y, sizeof(int));
+		close(fd[0]);
+		printf("got from child: %d\n", y);
+		wait(NULL);
 	}
 	(void)argv;
 	return (0);
