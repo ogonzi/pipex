021c2c908d4ebd27360fff4c8efa47a7c7b851da
Oscar Gonzalez simon <ogonzale@car1s4.42barcelona.com>
2022-08-10 17:11:02 +0200

    Separate functions in get_path

--- src/mand/get_path.c
+++ src/mand/get_path.c
@@ -6,49 +6,60 @@
 /*   By: ogonzale <ogonzale@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 16:01:24 by ogonzale          #+#    #+#             */
-/*   Updated: 2022/08/09 18:13:42 by ogonzale         ###   ########.fr       */
+/*   Updated: 2022/08/10 17:10:09 by ogonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-void	ft_get_command(char **cmd, char **options, char *argv_cmd, char *env[])
+void	ft_get_paths(char ***paths, char *env[])
 {
-	char	**paths;
-	char	*path_line;
-	char	*path_with_bar;
-	char	*command;
 	int		i;
-	int		j;
-
+	char	*path_line;
+	
 	i = 0;
-	paths = NULL;
 	while (env[i] != NULL)
 	{
 		if (ft_strnstr(env[i], "PATH", ft_strlen(env[i])) != NULL)
 		{
 			path_line = ft_substr(env[i], 5, ft_strlen(env[i]) - 5);
-			paths = ft_split(path_line, ':');
+			*paths = ft_split(path_line, ':');
 			free(path_line);
 			break ;
 		}
 		i++;
 	}
+}
+
+void	ft_split_argv(char **command, char **options, char *argv_cmd)
+{
+	int	i;
+
 	i = 0;
-	command = NULL;
-	*options = NULL;
-	j = 0;
-	while (j < (int)ft_strlen(argv_cmd))
+	while (i < (int)ft_strlen(argv_cmd))
 	{
-		if (argv_cmd[j] == ' ')
+		if (argv_cmd[i] == ' ')
 		{
-			command = ft_substr(argv_cmd, 0, j);
-			*options = ft_substr(argv_cmd, j + 1, ft_strlen(argv_cmd) - j);
+			*command = ft_substr(argv_cmd, 0, i);
+			*options = ft_substr(argv_cmd, i + 1, ft_strlen(argv_cmd) - i);
 			break ;
 		}
-		j++;
+		i++;
 	}
+
+}
+
+void	ft_get_command(char **cmd, char **options, char *argv_cmd, char *env[])
+{
+	char	**paths;
+	char	*path_with_bar;
+	char	*command;
+	int		i;
+
+	ft_get_paths(&paths, env);
+	ft_split_argv(&command, options, argv_cmd);
+	i = 0;
 	while (paths[i] != NULL)
 	{
 		path_with_bar = ft_strjoin(paths[i], "/");
